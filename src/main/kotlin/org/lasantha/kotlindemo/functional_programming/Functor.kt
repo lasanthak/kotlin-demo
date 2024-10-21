5715898b2684aab3376f1c213d5696d08a2e8771
Lasantha Kularatne <lasanthak@gmail.com>
2024-10-21 09:56:11 -0500

    Functors

---
+++ src/main/kotlin/org/lasantha/kotlindemo/functional_programming/Functor.kt
@@ -0,0 +1,26 @@
+package org.lasantha.kotlindemo.functional_programming
+
+/**
+ * Functors:
+ * Let C and D be categories. A functor F from C to D is a mapping that:
+ *  - associates with each object X in C an object F(X) in D
+ *  - associates with each morphism f: X -> Y in C a morphism F(f): F(X) -> F(Y) in D such that the following two conditions hold:
+ *    1. F(id_X) = id_F(X) for every object X in C
+ *    2. F(g ∘ f) = F(g) ∘ F(f) for all morphisms f: X -> Y and g: Y -> Z in C
+ *
+ * Functor laws:
+ * 1. Functors must preserve identity morphisms
+ * 2. Functors must preserve composition of morphisms
+ *
+ * Example:
+ * A simple class to demonstrate the functor laws.
+ *
+ * data class Triplet<A>(val a: A, val b: A, val c: A) : Functor<A> {
+ *     override fun <B> map(f: (A) -> B): Triplet<B> {
+ *         return Triplet(f(a), f(b), f(c))
+ *     }
+ * }
+ */
+interface Functor<A> {
+    fun <B> map(f: (A) -> B): Functor<B>
+}
---
+++ src/test/kotlin/org/lasantha/kotlindemo/functional_programming/FunctorTest.kt
@@ -0,0 +1,32 @@
+package org.lasantha.kotlindemo.functional_programming
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+data class Triplet<A>(val a: A, val b: A, val c: A) : Functor<A> {
+    override fun <B> map(f: (A) -> B): Triplet<B> {
+        return Triplet(f(a), f(b), f(c))
+    }
+}
+
+class FunctorTest {
+
+    @Test
+    fun `Identity Test`() {
+        // Functors must preserve identity morphisms
+        val t1 = Triplet(1, 2, 3)
+        val t2 = t1.map { it } // F(id_X)
+        assertEquals(t1, t2)
+    }
+
+    @Test
+    fun `Composition Test`() {
+        // Functors must preserve composition of morphisms
+        val f:(Int) -> Double = { it / 2.0 }
+        val g:(Double) -> String = { "${it}D" }
+        val t = Triplet(1, 2, 3)
+        val t1 = t.map { g(f(it)) } // F(g ∘ f)
+        val t2 = t.map(f).map(g) // F(g) ∘ F(f)
+        assertEquals(t1, t2)
+    }
+}
